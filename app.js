7ddd2f72bea193c55ca4612d85b6506923bd3171
Jiya Sharma <sjiya7527@gmail.com>
2025-05-25 19:04:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-// const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -32,7 +31,7 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
-// const dbUrl = process.env.ATLASDB_URL;
+
 
 async function main() {
     await mongoose.connect(MONGO_URL);
@@ -42,18 +41,6 @@ main().then(()=>{
     console.log("connected to DB");
 })
 .catch(err => console.log(err));
-
-// const store = MongoStore.create({
-//     mongoUrl:dbUrl,
-//     crypto: {
-//         secret: process.env.SECRET,
-//     },
-//     touchAfter: 24*3600,
-// });
-
-// store.on("error",()=>{
-//     console.log("ERROR in MONGO SESSION STORE",err);
-// });
 
 const sessionOptions ={
     // store: store,
@@ -67,9 +54,7 @@ const sessionOptions ={
     },
 };
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi, I am root");
-// });
+
 
 app.use(session(sessionOptions));
 app.use(flash());
